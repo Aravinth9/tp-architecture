ede79baf1ca1fb0d481cc14890ec5f2a4e28fa10
lnassim <35958653+lnassim@users.noreply.github.com>
2020-12-14 17:58:11 +0100

    Update and rename reservation.java to Reservation.java

--- SI2-Warriors/project/code/src/main/java/com/archi/rest/Reservation.java
+++ SI2-Warriors/project/code/src/main/java/com/archi/rest/Reservation.java
@@ -1,10 +1,10 @@
 package com.archi.rest;
 
-public class reservation {
+public class Reservation {
     int numero_reservation;
     boolean reserved;
 
-    public reservation(int num, boolean reserved)
+    public Reservation(int num, boolean reserved)
     {
         this.numero_reservation=num;
         this.reserved=reserved;
